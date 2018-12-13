e206d18d72f5c78a0059386afb3536c9ab0f8e7a
John Walker Moosbrugger <JohnWalker@Johns-MacBook-Pro.local>
2018-12-12 19:51:43 -0500

    vuex

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="footer">
         <div class="footer__nav">
-
+            <div class="footer__nav--container">
+                <h3 class="footer__nav--header">Design Your CubeRover</h3>
+            </div>
         </div>
         <p class="footer__copy">Copyright 2018</p>
     </div>
